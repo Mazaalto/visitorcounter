d9964ebb9ba9aa8dcd3a05b1001bc8c185a81413
Matias Aalto-Setälä <matias.aaltosetala@gmail.com>
2021-04-01 13:07:23 +0300

    Harjoitellaan kurssin ohjeiden mukaisesti laajempaa sovellusta, jossa sisäänkirjautuminen toteutettu

--- routes.py
+++ routes.py
@@ -6,5 +6,38 @@ from flask import render_template
 def index():
     visits.add_visit()
     counter = visits.get_counter()
-    return render_template("index.html", counter=counter)
+    list = messages.get_list()
+    return render_template("index.html", counter=counter, count=len(list), messages=list)
 
+@app.route("/send", methods=["POST"])
+def send():
+    content = request.form["content"]
+    if messages.send(content):
+        return redirect("/")
+    else:
+        return render_template("error.html",message="Viestin lähetys ei onnistunut")
+
+@app.route("/login", methods=["GET","POST"])
+def login():
+    if request.method == "GET":
+        return render_template("login.html")
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        if users.login(username,password):
+            return redirect("/")
+        else:
+            return render_template("error.html",message="Väärä tunnus tai salasana")
+
+# uuden käyttäjän rekisteröiminen sovellukseen
+@app.route("/register", methods=["GET","POST"])
+def register():
+    if request.method == "GET":
+        return render_template("register.html")
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        if users.register(username,password):
+            return redirect("/")
+        else:
+            return render_template("error.html",message="Rekisteröinti ei onnistunut")
